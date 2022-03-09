ad70f370057378b56951ebd98b8e01f833fa7f38
Oguzhan KILIC <shinobi7755@gmail.com>
2022-03-09 10:57:49 +0100

    Adding properties
    Adding properties for eureka

--- Code/src/test/java/com/bluelagoon/netflix/NetflixApplicationTests.java
+++ Code/src/test/java/com/bluelagoon/netflix/NetflixApplicationTests.java
@@ -2,6 +2,7 @@ package com.bluelagoon.netflix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
 class NetflixApplicationTests {
