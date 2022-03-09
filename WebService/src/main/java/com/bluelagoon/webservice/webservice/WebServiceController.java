ee4a2b58e7bfc365df6d598311cc66d2b5160117
Oguzhan KILIC <shinobi7755@gmail.com>
2022-03-09 11:33:09 +0100

    Update adding Actuator

---
+++ WebService/src/main/java/com/bluelagoon/webservice/webservice/WebServiceController.java
@@ -0,0 +1,13 @@
+package com.bluelagoon.webservice.webservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello";
+    }
+}
