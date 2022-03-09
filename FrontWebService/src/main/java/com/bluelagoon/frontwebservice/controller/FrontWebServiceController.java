3ed4798c9f770a91fc050f4059e04915cee4417f
Olivier Bastin <bastin.olivier77@gmail.com>
2022-03-09 15:17:30 +0100

    2 service work

---
+++ FrontWebService/src/main/java/com/bluelagoon/frontwebservice/controller/FrontWebServiceController.java
@@ -0,0 +1,34 @@
+package com.bluelagoon.frontwebservice.controller;
+
+import org.slf4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Locale;
+
+
+@RestController
+    public class FrontWebServiceController {
+        @Autowired
+        DiscoveryClient discoveryClient;
+        @GetMapping("/")
+        public String hello() {
+            List<ServiceInstance> instances = discoveryClient.getInstances("NETFLIX_MICRO_SERVICE");
+            ServiceInstance test = instances.get(0);
+            String hostname = test.getHost();
+            int port = test.getPort();
+            RestTemplate restTemplate = new RestTemplate();
+            String microservice1Address = "http://" + hostname + ":" + port;
+            ResponseEntity<String> response =
+                    restTemplate.getForEntity(microservice1Address, String.class);
+            String s = response.getBody().toUpperCase(Locale.ROOT);
+            return s;
+        }
+    }
